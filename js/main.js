583f505e9ca4bc739356cda24b6ce67fb12911e8
Andrew Toups <mistertoups@gmail.com>
2016-10-04 20:45:27 -0400

    added error checking and message if valid name has no values

--- js/main.js
+++ js/main.js
@@ -5,28 +5,43 @@ button.click(function(){
   gitQuery();
 });
 
+function handleError(errorObject, textStatus, error) {
+  var errorMsg = $('<div>').attr('class', 'org-container');
+  errorMsg = errorMsg.text('"' +textStatus+ ": " +error+ '"');
+  $(errorMsg).appendTo('.container');
+  console.log(errorObject, textStatus, error);
+}
+
 function gitQuery(){
   $('.container').empty();
   var username = input.val();
-  var key = 'de8d63686f9c21f55aa05b1c8365791379eb51b7';
+  var key = 'a319189ce3b53d7d45cb008065c75faf9e2fa8a1';
   var settings = {
     async: true,
     crossDomain: true,
     url: "https://api.github.com/users/"+ username +"/orgs?access_token=" + key,
     method: "GET",
     processData: false,
-    data: "{}"
+    data: "{}",
+    error: handleError
   };
   $.ajax(settings).done(function(response) {
+    if (index === undefined) {
+      index = 0; //create an org-like object with dummy values
+      response[index] = {
+        login: "Nothing to display!",
+        avatar_url: "",
+        description: ""
+      };
+    }
     for (var index in response){
-      console.log(response[index]);
-      new displayOrg(response[index]);
+    new displayOrg(response[index]);
     }
   });
 }
 
 function displayOrg(org){
-  // console.log("here we are");
+
   this.info = {
     name: org.login,
     img: org.avatar_url,
@@ -34,6 +49,7 @@ function displayOrg(org){
   };
 
   this.createElements = function() {
+
     var orgContainer = $('<div>').attr('class', 'org-container');
     var img = $('<img>').attr('src', this.info.img).appendTo(orgContainer);
     var span = $('<span>').text(this.info.name).appendTo(orgContainer);
